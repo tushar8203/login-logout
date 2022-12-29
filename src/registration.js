438d6b775b41cb02f49c9fc3232e4f9825b690f7
tushar8203 <tushar08chauhan@gmail.com>
2022-12-29 12:58:58 +0530

    updation

--- src/registration.js
+++
@@ -1,8 +0,0 @@
-let mongoose=require("mongoose");
-let signSchema=mongoose.Schema({
-    Name:String,
-    Email:String,
-    Password:String
-})
-let sign=mongoose.model("sign", signSchema,"Sign");
-module.exports=sign;
